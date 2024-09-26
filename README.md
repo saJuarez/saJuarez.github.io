5084a68df179439df85184259b31b92fc9d219cf
saJuarez <sajuarez@uncg.edu>
2024-09-25 23:52:36 -0400

    Articles & associated javascript added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # My GitHub Pages site
+
+
+## Citations
+https://www.google.com/maps/uv?pb=!1s0x8853196a68acc9d5%3A0xc10dcb0cf5825682!3m1!7e115!5sGoogle%20Search!15sCgIgAQ&cr=lr_f3&hl=en&imagekey=!1e10!2sAF1QipP5mcq4dJcpWP7qiW6__Xo69NN3p7AQya_VKd6-&sa=X&ved=2ahUKEwiHvcOapd-IAxWwq4kEHSIjFpYQ9fkHKAB6BAgBECE
+
+https://lh3.googleusercontent.com/p/AF1QipPo95hRpHuGOmjYPVlyVB7yFXqnZmfYZ0ilK-LU=s1360-w1360-h1020
--- csc372-hw/hw3/index.js
+++ csc372-hw/hw3/index.js
@@ -1,10 +1,47 @@
-/* Script for index.html */
+/* Javascript for index.html */
 document.addEventListener('DOMContentLoaded', function() {
-    const articlePreview = document.getElementById('article1');
-    const mainContainer = document.querySelector('main');
+    const articlePreviews = document.querySelectorAll('.article-preview');
+    const mainElement = document.querySelector('main');
 
-    articlePreview.addEventListener('click', function() {
-        this.classList.toggle('expanded');
-        mainContainer.classList.toggle('main-expanded');
+    articlePreviews.forEach(article => {
+        const mainImage = article.querySelector('.main-image');
+        const articleText = article.querySelector('.article-text p');
+
+        const initialDescriptionId = mainImage.getAttribute('data-description-id');
+        const initialDescription = document.getElementById(initialDescriptionId).textContent;
+        articleText.textContent = initialDescription;
+
+        article.addEventListener('click', function() {
+            const isExpanded = this.classList.contains('expanded');
+
+            articlePreviews.forEach(a => a.classList.remove('expanded'));
+            mainElement.classList.remove('main-expanded');
+
+            if (!isExpanded) {
+                this.classList.add('expanded');
+                mainElement.classList.add('main-expanded');
+            }
+        });
+
+        const thumbnails = article.querySelectorAll('.thumbnail');
+
+        thumbnails.forEach(thumbnail => {
+            thumbnail.addEventListener('click', function(event) {
+                event.stopPropagation(); 
+
+                const tempSrc = mainImage.src;
+                mainImage.src = this.src;
+                this.src = tempSrc;
+
+                const descriptionId = this.getAttribute('data-description-id');
+                const mainImageDescriptionId = mainImage.getAttribute('data-description-id');
+
+                const description = document.getElementById(descriptionId).textContent;
+                articleText.textContent = description; 
+
+                mainImage.setAttribute('data-description-id', descriptionId);
+                this.setAttribute('data-description-id', mainImageDescriptionId);
+            });
+        });ss
     });
 });
